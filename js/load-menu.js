8fbb098cf81c98a53edd4da475872e49cb37b914
Maysoloprueba <mayicevallosvargas@gmail.com>
2025-07-29 09:36:53 -0400

    lo que hizoi e profe

---
+++ js/load-menu.js
@@ -0,0 +1,15 @@
+fetch("./partials/menu.html")
+  .then(res => {
+    if (!res.ok) throw new Error("No se pudo cargar el menú");
+    return res.text();
+  })
+  .then(data => {
+    console.log('Contenido recibido del menú:', data); // ✅ debug
+    const container = document.getElementById('menu-container');
+    if (container) {
+      container.innerHTML = data;
+    }
+  })
+  .catch(err => {
+    console.error('Error al cargar el menú:', err);
+  });
