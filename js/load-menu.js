2408aa99a999bccaab95cf03d7c912430b248abd
Dev17 MasBlue <a.galea@masblue.cl>
2025-10-02 10:20:02 -0300

    subo posible menu de los cursos

--- js/load-menu.js
+++ js/load-menu.js
@@ -1,10 +1,12 @@
-fetch("./partials/menu.html")
+
+
+  fetch("./partials/menu.html")
   .then(res => {
     if (!res.ok) throw new Error("No se pudo cargar el menú");
     return res.text();
   })
   .then(data => {
-    console.log('Contenido recibido del menú:', data); // ✅ debug
+    console.log('Contenido recibido del menú:', data);
     const container = document.getElementById('menu-container');
     if (container) {
       container.innerHTML = data;
